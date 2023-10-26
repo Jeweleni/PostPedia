ecac13e55b76897d6d491d17ae7fb0557cc6b0b4
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2023-10-26 10:53:22 +0100

    layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        purple: {
+          50: '#f7f3ff',
+          100: '#e9d8ff',
+          200: '#d6bcff',
+          300: '#c399ff',
+          400: '#a274ff',
+          500: '#9147ff',
+          600: '#7f2eea',
+          700: '#6a24d8',
+          800: '#571dc3',
+          900: '#4713a5',
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
