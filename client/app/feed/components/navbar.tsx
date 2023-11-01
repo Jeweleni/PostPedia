fb29fcebf8d5a2bcd740a63e445236ad058deea1
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2023-11-01 12:56:04 +0100

    for you

--- client/app/feed/components/navbar.tsx
+++ client/app/feed/components/navbar.tsx
@@ -19,12 +19,13 @@ import { AiOutlineBell, AiOutlineSearch } from "react-icons/ai";
     const isSidebarOpen = false;
   
     return (
-      <div className="w-full h-fit self-stretch p-2 border border-stone-300 justify-end items-start gap-[189px] inline-flex">
-        {isSidebarOpen ? ( // Check if the sidebar is open
-          // Show only the search bar when the sidebar is open
-          <div className="w-fit self-stretch rounded-lg border border-stone-300 justify-start items-center gap-6 inline-flex">
-            <div className="px-3 py-2 justify-center items-center gap-2 flex">
-              <AiOutlineSearch />
+
+      <div className="w-full h-fit self-stretch p-3 border border-stone-300 items-center inline-flex justify-center gap-80">
+        {/* {isSidebarOpen ? ( // Check if the sidebar is open */}
+  
+          <div className="rounded-lg border border-stone-300 w-5/12 items-start">
+            <div className="p-2 justify-start items-start gap-2 flex">
+              <AiOutlineSearch className=" text-zinc-600 w-6 h-6"/>
               <span className="text-zinc-600 text-lg font-normal font-['DM Sans'] leading-[27px]">
                 <input
                   type="text"
@@ -41,17 +42,15 @@ import { AiOutlineBell, AiOutlineSearch } from "react-icons/ai";
               </span>
             </div>
           </div>
-        ) : (
-          // Show User Profile Image and notifications when the sidebar is closed
-          <div className="self-stretch justify-end gap-5 inline-flex mr-2">
-            <div className="w-12 h-12 relative font-light">
-              <AiOutlineBell className="w-12 h-12" />
+          <div className="self-stretch gap-5 inline-flex mr-3">
+            <div className="w-10 h-10 relative font-light">
+              <AiOutlineBell className="w-10 h-10" />
             </div>
-            <div className="w-12 h-12 relative">
+            <div className="w-10 h-10 relative">
               <div className="bg-zinc-300 rounded-full">
                 <Image
                   src={userprofile}
-                  className="w-12 h-12 rounded-full"
+                  className="w-10 h-10 rounded-full"
                   alt="User Profile"
                   width={52.68}
                   height={51.34}
@@ -59,8 +58,8 @@ import { AiOutlineBell, AiOutlineSearch } from "react-icons/ai";
               </div>
             </div>
           </div>
-        )}
-      </div>
+          </div>
+ 
     );
   };
   
--- client/app/feed/pages/feed.tsx
+++ client/app/feed/pages/feed.tsx
@@ -1,14 +1,19 @@
 'use client'
 import React, { useState } from "react";
 import FeedTabsSection from "./FeedTabsSection";
+import { Button } from "@nextui-org/react";
+import { FiEdit2 } from 'react-icons/fi';
+import ForYouPage from "./ForYou";
 
 const FeedPage: React.FC = () => {
   const [activeTab, setActiveTab] = useState("for-you");
 
   return (
-    <div className="w-fit h-auto relative border border-stone-300 p-3">
-      <div className="p-6 w-fit gap-80 inline-flex justify-start items-center ">
-        <div className="flex-col justify-start items-start gap-3 inline-flex">
+    <div className="w-full h-auto rounded-lg border border-stone-300 justify-between items-center">
+    <div className="p-6 justify-between w-full self-stretch">
+    <div className="p-6 w-full h-auto relative border border-stone-300">
+      <div className=" p-6 w-full gap-80 inline-flex justify-start items-center ">
+        <div className="flex-col justify-start items-start gap-3 inline-flex p-6">
           <div className="text-black text-[32px] font-medium font-['DM Sans'] leading-[48px]">
             FEED
           </div>
@@ -16,28 +21,45 @@ const FeedPage: React.FC = () => {
             Explore different content you’d love
           </div>
         </div>
-        <div className="w-[177px] px-4 py-2 rounded-lg justify-center items-center gap-2 flex">
+        {/* <div className="w-fit px-4 py-2 rounded-lg justify-center items-center gap-2 flex">
           <div className="w-6 h-6 relative">
-            <div className="w-[19.80px] h-[19.80px] left-[2.66px] top-[1.95px] absolute"></div>
+          </div>
+          <Button className="bg-primary text-white font-bold font-['DM Sans'] leading-normal ">
+            Post a content
+          </Button>
+        </div> */}
+        <div className="justify-between">
+        <Button className="w-[177px] h-14 bg-primary rounded-lg items-center gap-2 inline-flex">
+          <div className="w-6 h-6 relative">
+            <div className="w-[19.80px] h-[19.80px] left-[2.66px] top-[1.95px] text-white absolute "><FiEdit2/></div>
           </div>
           <div className="text-white text-base font-bold font-['DM Sans'] leading-normal">
             Post a content
           </div>
+        </Button>
         </div>
-
-        
       </div>
-      <FeedTabsSection setActiveTab={setActiveTab} activeTab={activeTab} />
-
-        {/* Content for "For You" section */}
-        {activeTab === "for-you" && <div id="for-you" className="">For You content</div>}
-
-        {/* Content for "Featured" section */}
-        {activeTab === "featured" && <div id="featured">Featured content</div>}
-
-        {/* Content for "Recent" section */}
-        {activeTab === "recent" && <div id="recent">Recent content</div>}
+      
+    
+   
+    <div className="p-6">
+    <FeedTabsSection setActiveTab={setActiveTab} activeTab={activeTab} />
     </div>
+   <div className="p-6">
+    {activeTab === "for-you" && (
+      <div id="for-you" >
+        <ForYouPage/>
+      </div>
+    )}
+
+    {activeTab === "featured" && <div id="featured" className="w-fit h-auto relative rounded-lg border border-stone-300 p-6">Featured content</div>}
+
+    {activeTab === "recent" && <div id="recent" className="w-fit h-auto relative rounded-lg border border-stone-300 p-6">Recent content</div>}
+  </div>
+  </div>
+  </div>
+
+  </div>
   );
 };
 
