fb3f9d417d1ff2e90a8fe345b41b4d258cc50182
Erickb <wr.t8r1@gmai.com>
2023-11-03 11:15:58 -0700

    Settting up auht

---
+++ server/prisma/migrations/20231103112609_default_fix/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `identifier` on the `VerificationToken` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[id,token]` on the table `VerificationToken` will be added. If there are existing duplicate values, this will fail.
+  - The required column `id` was added to the `VerificationToken` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+
+*/
+-- DropIndex
+DROP INDEX "VerificationToken_identifier_token_key";
+
+-- AlterTable
+ALTER TABLE "VerificationToken" DROP COLUMN "identifier",
+ADD COLUMN     "id" TEXT NOT NULL,
+ADD CONSTRAINT "VerificationToken_pkey" PRIMARY KEY ("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_id_token_key" ON "VerificationToken"("id", "token");
